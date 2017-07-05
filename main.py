90f705ec6dd108d76a09524fa8d689cabf6fb1f8
cclauss <cclauss@bluewin.ch>
2017-07-05 21:52:56 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import inspect
+import os
+import platform
+import sys
+
+for name, value in inspect.getmembers(platform):
+    if name[0] != '_' and callable(value):
+        try:
+            value = value()
+        except (IndexError, TypeError):
+            continue
+        if str(value).strip("( ,')"):
+            print('{:>21}() = {}'.format(name, value))
+
+print(sys.platform, sys.version)
+print(os.environs)
